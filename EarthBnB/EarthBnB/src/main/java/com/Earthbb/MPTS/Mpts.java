99e8b358326325a27f935ed3959248b81ae61925
Michal Terranova <MTerranova@talentpath.com>
2020-09-02 11:16:26 -0400

    file restructuring, routing added

---
+++ EarthBnB/EarthBnB/src/main/java/com/Earthbb/MPTS/Mpts.java
@@ -0,0 +1,24 @@
+package com.Earthbb.MPTS;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Mpts {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
