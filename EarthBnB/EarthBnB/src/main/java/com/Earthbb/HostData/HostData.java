4835aa9c4f1b5a0d6050857486569979e4946034
avalojc <55007242+avalojc@users.noreply.github.com>
2020-09-04 00:19:37 -0400

    Seed host db and create routes (#28)
    * seeded data
    
    * created routes for host data (entity, controller, repo)

---
+++ EarthBnB/EarthBnB/src/main/java/com/Earthbb/HostData/HostData.java
@@ -0,0 +1,114 @@
+package com.Earthbb.HostData;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+
+@Table(name="hostdata")
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class HostData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private int joindate;
+    private boolean idverifieded;
+    private String biography;
+    private String cohosts;
+    private String duringstay;
+    private String communication;
+    private int responserate;
+    private int responsetime;
+
+    public HostData(Long id, int joindate, boolean idverifieded, String biography, String cohosts, String duringstay, String communication, int responserate, int responsetime) {
+        this.id = id;
+        this.joindate = joindate;
+        this.idverifieded = idverifieded;
+        this.biography = biography;
+        this.cohosts = cohosts;
+        this.duringstay = duringstay;
+        this.communication = communication;
+        this.responserate = responserate;
+        this.responsetime = responsetime;
+    }
+
+    public HostData() {
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getJoindate() {
+        return joindate;
+    }
+
+    public void setJoindate(int joindate) {
+        this.joindate = joindate;
+    }
+
+    public boolean isIdverifieded() {
+        return idverifieded;
+    }
+
+    public void setIdverifieded(boolean idverifieded) {
+        this.idverifieded = idverifieded;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public String getCohosts() {
+        return cohosts;
+    }
+
+    public void setCohosts(String cohosts) {
+        this.cohosts = cohosts;
+    }
+
+    public String getDuringstay() {
+        return duringstay;
+    }
+
+    public void setDuringstay(String duringstay) {
+        this.duringstay = duringstay;
+    }
+
+    public String getCommunication() {
+        return communication;
+    }
+
+    public void setCommunication(String communication) {
+        this.communication = communication;
+    }
+
+    public int getResponserate() {
+        return responserate;
+    }
+
+    public void setResponserate(int responserate) {
+        this.responserate = responserate;
+    }
+
+    public int getResponsetime() {
+        return responsetime;
+    }
+
+    public void setResponsetime(int responsetime) {
+        this.responsetime = responsetime;
+    }
+
+
+}
