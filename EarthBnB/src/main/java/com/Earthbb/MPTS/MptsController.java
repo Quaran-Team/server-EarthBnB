820fc415e939a12da533943dbc966abcd8778763
Michal Terranova <MTerranova@talentpath.com>
2020-09-08 11:45:09 -0400

    edits to files

--- EarthBnB/src/main/java/com/Earthbb/MPTS/MptsController.java
+++ EarthBnB/src/main/java/com/Earthbb/MPTS/MptsController.java
@@ -3,9 +3,12 @@ package com.Earthbb.MPTS;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
+@CrossOrigin(origins = { "http://localhost:4200" })
 @RestController
 @RequestMapping("/api/v1/mpts")
 public class MptsController {
@@ -25,4 +28,27 @@ public class MptsController {
 
     @DeleteMapping("/{id}")
     void delete(@PathVariable Long id) { mptsRepository.deleteById(id);}
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Mpts> update(@PathVariable(value = "id") Long id,
+                                       @RequestBody Mpts mptsDetails) throws ResourceNotFoundException {
+        Mpts mpts = mptsRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Sorry, we don't have that as your next get away!"));
+        mpts.setRating(mptsDetails.getRating());
+        mpts.setRating_amount(mptsDetails.getRating_amount());
+        mpts.setSuperhost(mptsDetails.getSuperhost());
+        mpts.setPrice(mptsDetails.getPrice());
+        mpts.setAddress(mptsDetails.getAddress());
+        mpts.setCity(mptsDetails.getCity());
+        mpts.setState_territory(mptsDetails.getState_territory());
+        mpts.setCountry(mptsDetails.getCountry());
+        mpts.setZip(mptsDetails.getZip());
+        mpts.setInternational(mptsDetails.getInternational());
+        mpts.setHost(mptsDetails.getHost());
+        mpts.setTitle(mptsDetails.getTitle());
+        mpts.setBeds(mptsDetails.getBeds());
+        mpts.setBedrooms(mptsDetails.getBedrooms());
+        mpts.setBathrooms(mptsDetails.getBathrooms());
+        final Mpts updated = mptsRepository.save(mpts);
+        return ResponseEntity.ok(updated);
+    }
 }
