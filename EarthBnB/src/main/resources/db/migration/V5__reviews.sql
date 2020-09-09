2bfbec1483c1284def84a16adfcba4b23e36b301
Trevis42 <sirtrevis42@gmail.com>
2020-09-09 08:45:06 -0500

    fixed sql syntax error

--- EarthBnB/src/main/resources/db/migration/V5__reviews.sql
+++ EarthBnB/src/main/resources/db/migration/V5__reviews.sql
@@ -1,16 +1,16 @@
-CREATE TABLE reviews (
-    id BIGINT NOT NULL AUTO_INCREMENT,
-    username VARCHAR(255),
-    userprofile VARCHAR(255),
-    reviewtext VARCHAR(255),
-    category_cleanliness DECIMAL(2, 1),
-    category_accuracy DECIMAL(2, 1),
-    category_communication DECIMAL(2, 1),
-    category_location DECIMAL(2, 1),
-    category_checkin DECIMAL(2, 1),
-    category_value DECIMAL(2, 1),
-    posteddate VARCHAR(255),
-    locationreviewed BIGINT,
-    FOREIGN KEY (locationreviewed) REFERENCES locations(id),
-    PRIMARY KEY (id)
-);
+CREATE TABLE `reviews` (
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `username` TEXT,
+    `userprofile` TEXT,
+    `reviewtext` TEXT,
+    `category_cleanliness` DECIMAL(2, 1),
+    `category_accuracy` DECIMAL(2, 1),
+    `category_communication` DECIMAL(2, 1),
+    `category_location` DECIMAL(2, 1),
+    `category_checkin` DECIMAL(2, 1),
+    `category_value` DECIMAL(2, 1),
+    `posteddate` TEXT,
+    `locationreviewed` BIGINT,
+    FOREIGN KEY (`locationreviewed`) REFERENCES `locations` (`id`),
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
