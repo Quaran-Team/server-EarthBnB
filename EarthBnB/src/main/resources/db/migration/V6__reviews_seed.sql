af1fd287950894bfe24b3088f91046125eafce57
Trevis42 <sirtrevis42@gmail.com>
2020-09-09 09:54:17 -0500

    added more seeded data

--- EarthBnB/src/main/resources/db/migration/V6__reviews_seed.sql
+++ EarthBnB/src/main/resources/db/migration/V6__reviews_seed.sql
@@ -1,10 +1,52 @@
 INSERT INTO `reviews` (
         `username`,
         `userprofile`,
-        `reviewtext`
+        `reviewtext`,
+        `category_cleanliness`,
+        `category_accuracy`,
+        `category_communication`,
+        `category_location`,
+        `category_checkin`,
+        `category_value`,
+        `posteddate`,
+        `locationreviewed`
     )
 VALUES (
         "Trevor",
         "Trevor Profile link",
-        "This place was awesome. We had a lot of fun at local entertainment spots. Worth checking out."
+        "This place was awesome. We had a lot of fun at local entertainment spots. Worth checking out.",
+        5.0,
+        4.7,
+        4.9,
+        5.0,
+        4.7,
+        4.8,
+        "September 2020",
+        2
+    ),
+    (
+        "Rawrzor",
+        "Rawrzor Profile link",
+        "Meh, it was alright. Not much to do around the area. Place was kinda messy. Nice, friendly service and check-in",
+        3.8,
+        4.2,
+        4.1,
+        3.3,
+        4.6,
+        3.7,
+        "June 2020",
+        3
+    ),
+    (
+        "Lauren",
+        "Lauren Profile link",
+        "This place was really neat. Had fun exploring the area. Friendly host.",
+        5.0,
+        4.7,
+        4.9,
+        5.0,
+        4.7,
+        4.8,
+        "January 2020",
+        2
     );
