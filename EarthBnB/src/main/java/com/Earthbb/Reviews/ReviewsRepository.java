c2ac6e50f5c7d04b169973d4f3c01cbf062fdd0f
Trevis42 <sirtrevis42@gmail.com>
2020-09-17 10:29:43 -0500

    cleaned up unused imports

--- EarthBnB/src/main/java/com/Earthbb/Reviews/ReviewsRepository.java
+++ EarthBnB/src/main/java/com/Earthbb/Reviews/ReviewsRepository.java
@@ -1,11 +1,8 @@
 package com.Earthbb.Reviews;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface ReviewsRepository extends JpaRepository<Reviews, Long> {
     
