71bc9e614994a0ef18739ebde35b6d46121ed7be
BLoveless <bloveless@talentpath.com>
2020-09-24 12:26:38 -0400

    fixed price data type and added more listings changed a few pics

--- EarthBnB/src/main/resources/db/migration/V11__todo_table.sql
+++ EarthBnB/src/main/resources/db/migration/V11__todo_table.sql
@@ -1,11 +1,11 @@
 create table todo (
 id bigint not null auto_increment,
 img varchar(300),
-rating decimal,
+rating decimal(6,3),
 reviews int(10),
 title varchar(100),
 category varchar(100),
-price decimal,
+price decimal(12,4),
 location varchar(20),
 favorite boolean default false,
 info varchar(800),
